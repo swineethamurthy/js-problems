c69153567dc19b92ab0927e255048fda7989e806
swineethamurthy <swineethamurthy@gmail.com>
2022-08-25 10:31:11 +0530

    fix: simplified redundant code

--- assets/js/find-largest-number.js
+++ assets/js/find-largest-number.js
@@ -3,20 +3,10 @@ function onButtonClick() {
 
   const listSize = prompt("Enter size of numbers you would like to compare");
 
-  const number1 = prompt("Enter a number");
-  numbersList.push(parseInt(number1));
-  const number2 = prompt("Enter next number");
-  numbersList.push(parseInt(number2));
-
   let largestNumber = 0;
-  if (number1 > number2) {
-    largestNumber = number1;
-  } else {
-    largestNumber = number2;
-  }
 
-  for (let counter = 0; counter < listSize - 2; counter++) {
-    const newNumber = parseInt(prompt("Enter next number"));
+  for (let counter = 0; counter < listSize; counter++) {
+    const newNumber = parseInt(prompt("Enter the number"));
     numbersList.push(newNumber);
     if (newNumber > largestNumber) {
       largestNumber = newNumber;
